9443d5da8de2f73686f61054ef0458c1fbcb290b
mramo21 <mantasramo@gmail.com>
2021-05-11 18:58:10 +0300

    string transform

---
+++ codewars/16-transforString.js
@@ -0,0 +1,14 @@
+const s1 = 'labas';
+const s2 = 'RYTAS';
+const s3 = 'KaS AnTrAs';
+
+const s1u = s1.toUpperCase();
+const s1l = s1.toLowerCase();
+
+const s2u = s2.toUpperCase();
+const s2l = s2.toLowerCase();
+
+const s3u = s3.toUpperCase();
+const s3l = s3.toLowerCase();
+
+
